3cce6c6e59a1a608987c9f56c21bf215db9bd13e
Nimrat Gill <nimratgill@Nimrats-MBP.lan>
2024-10-03 10:51:29 -0700

    add cors middleware

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors';
 import coinsRouter from './routes/coinsRouter.js'
 
 const app = express();
 const PORT = 8080;
 
 //middleware
+app.use(cors());
 app.use(express.json());
 
 //routes
